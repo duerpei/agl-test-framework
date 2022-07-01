e0eff64e1254599128df97a44a01c6f924515218
duerpei <duep.fnst@fujitsu.com>
2022-07-01 10:20:59 +0800

    update code
    Signed-off-by: duerpei <duep.fnst@fujitsu.com>

--- plugins/agl_test_log.py
+++ plugins/agl_test_log.py
@@ -5,49 +5,65 @@ import re
 from plugins.agl_test_conf import REPORT_LOGS_DIR
 from plugins.agl_test_conf import TMP_LOGS_DIR
 
-#Process the log like and get test_cases_values_and_status
-#log : the path of source log
-#mode : the type of log to process
-def log_process_default(log,mode):
-    #default log looks like :
-    #   -> case_name: TEST-PASS
-    #   -> case_name: TEST-FAIL
-    #   -> case_name: TEST-SKIP
-    pattern_default_line = re.compile('\w+: TEST-\w+')
-    pattern_default_id = re.compile("\w+(?=:)")
-    pattern_default_value = re.compile('TEST-\w{2,}')
+'''
+Process the log and init test_cases_values_and_status.
 
-    #gnome_desktop_testing result log, looks like:
-    #   PASS: glib/tls-database.test
-    #   FAIL: glib/markup-escape.test
-    #   SKIP: glib/testname.test
-    pattern_gnome_line = re.compile('[PFS][AK][SI][SLP]\w*:\B.+test')
-    pattern_gnome_id = re.compile('(?<=/).+')
-    pattern_gnome_value = re.compile('\w+(?=:)')
+log : the path of default log
 
-    pattern_list = [[pattern_default_line,pattern_default_id,pattern_default_value],[pattern_gnome_line,pattern_gnome_id,pattern_gnome_value]]
-    pattern_num = 0
-    if(mode == "gnome"):
-        pattern_num = 1
-
-    file = open(log)
-    count = 0
+default log formate :
+    -> case_name: TEST-PASS
+    -> case_name: TEST-FAIL
+    -> case_name: TEST-SKIP
+'''
+def log_process_default(log):
+    pattern = '^ -> (.+?): (.+?)$'
+    parse_result = log_parse(log, pattern)
     test_cases_values_and_status = [["test_id","values","status"]]
-    while True:
-        txt = file.readline()
-        if not txt:
-            break
-        res = re.search(pattern_list[pattern_num][0],txt)
-        if(res != None):
-            res = res.group()
-            test_id = re.search(pattern_list[pattern_num][1],res)
-            value = re.search(pattern_list[pattern_num][2],res)
-            if(value != None and test_id !=None):
-                l = [" "," "," "]
-                l[0] = test_id.group()
-                l[1] = value.group()
-                test_cases_values_and_status.append(l)
 
-    file.close()
+    if parse_result:
+        for item in parse_result:
+            item_result = [item[0], item[1], ""]
+            test_cases_values_and_status.append(item_result)
+
     return test_cases_values_and_status
+
+'''
+Process the log create by gnome_desktop_testing
+and init test_cases_values_and_status.
+
+log : the path of gnome_desktop_testing log
+
+gnome_desktop_testing log formate:
+    PASS: glib/tls-database.test
+    FAIL: glib/markup-escape.test
+    SKIP: glib/testname.test
+'''
+def log_process_gnome_desktop_testing(log):
+    pattern = '^(PASS|FAIL|SKIP.+?): (.+test?)$'
+    parse_result = log_parse(log, pattern)
+    test_cases_values_and_status = [["test_id","values","status"]]
+    
+    if parse_result:
+        for item in parse_result:
+            item_result = [item[1], item[0], ""]
+            test_cases_values_and_status.append(item_result)
+
+    return test_cases_values_and_status
+
+# parse log file with pattern
+def log_parse(log, pattern):
+    regex = re.compile(pattern, re.MULTILINE)
+
+    test_log = open(log, 'r')
+    
+    parse_result = []
+    line = test_log.readline()
+    while line:
+        matchs = regex.search(line)
+        if matchs:
+            groups = matchs.groups()
+            parse_result.append(groups)
+        line=test_log.readline()
+    test_log.close()
+    return parse_result
 
--- plugins/agl_test_utils.py
+++ plugins/agl_test_utils.py
@@ -12,13 +12,22 @@ def find_cmd(cmd):
     output.close()
     ret = val.find(cmd)
     if ret>=0:
-        print("{} is found: {}".format(cmd,val))
+        return 0
     else:
         print("error: {} is not found".format(cmd))
-
+        return 1
 
 #Make dir for THIS_TEST to save the log
 def create_dir(THIS_TEST):
     cmdline = "mkdir -p " + TMP_LOGS_DIR + THIS_TEST + "/log/; " + "mkdir -p " + REPORT_LOGS_DIR + ";"
     output = os.popen(cmdline)
     output.close()
+
+# print errors
+def printe(msg):
+    print("**** ERROR: " + msg)
+
+# print debug info
+def printd(msg):
+    # TODO
+    print("==== DEBUG: " + msg)
